c90adb9e63311c6b0182cfa52d06ba7b78fe4439
Wojciech <wojtekpako2@gmail.com>
2021-10-15 10:20:53 +0200

    lab1 zadanie 3

--- lab1.py
+++ lab1.py
@@ -2,3 +2,19 @@
 a = "Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker"
 print(a)
 #zad2
+
+
+
+
+
+
+
+
+#zad3
+from zad3_tekst import  tekst
+print('{:_>50}'.format(tekst))
+print('{:<50}'.format(tekst))
+print('{:^50}'.format(tekst))
+print('{:*^50}'.format(tekst))
+print('{:^6}'.format(tekst))
+
