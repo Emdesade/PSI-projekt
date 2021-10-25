cd5708148379b689b9b24552afb8925aa0acc76d
Wojciech <wojtekpako2@gmail.com>
2021-10-25 22:10:05 +0200

    zad4

--- lab2.py
+++ lab2.py
@@ -42,11 +42,30 @@ def zad3(text, letter):
     return text.replace(letter, "")
 
 
-textz3 = zad3("AAAAaaaaa to jest Napis Testujacy Dzialanie Funkcji z ZADANIA trzeciego", "a")
+textz3 = zad3("zzZzzzzzazzzdzzzznzzzizzzezzzzzzzzz zzz3zzzzzzzz", "z")
 print(f"zad3: {textz3}\n")
 
-# zad4
 
+# zad4
+def zad4(value, temperature_type):
+    if type(value) == int or type(value) == float:
+        if temperature_type == "Fahrenheit":
+            return round(value * 1.8 + 32, 2)
+        if temperature_type == "Rankine":
+            return round((value + 273.15) * 1.8, 2)
+        if temperature_type == "Kelvin":
+            return value + 273.15
+        else:
+            return "Błąd, sprawdź czy podane dane są prawidłowe"
+    else:
+        return "Błąd, sprawdź czy podane dane są prawidłowe"
+
+
+print(f"zad4a: 10`C -> Fahrenheit: {zad4(10, 'Fahrenheit')}")
+print(f"zad4b: 10`C -> Rankine: {zad4(10, 'Rankine')}")
+print(f"zad4c: 10`C -> Kelvin: {zad4(10, 'Kelvin')}")
+print(f"zad4d: test_błąd : {zad4(10, 'Delisle ')}")
+print(f"zad4e: test_błąd : {zad4('test', 'Kelvin ')}\n")
 
 # zad5
 
