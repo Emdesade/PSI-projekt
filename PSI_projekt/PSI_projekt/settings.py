1f77ac61a87cef254a642a06dd60345fd4413802
Wojciech <wojtekpako2@gmail.com>
2021-11-22 15:49:21 +0100

    add clothes to apps

--- PSI_projekt/PSI_projekt/settings.py
+++ PSI_projekt/PSI_projekt/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'clothes.apps.ClothesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
