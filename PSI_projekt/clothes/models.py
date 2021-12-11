d1fc38e4cba55369fea1d56082133d81ec823e37
Wojciech <wojtekpako2@gmail.com>
2021-12-11 11:11:58 +0100

    Django admin panel def __str__(self) added

--- PSI_projekt/clothes/models.py
+++ PSI_projekt/clothes/models.py
@@ -6,6 +6,9 @@ class Cloth_type(models.Model):
     id_cloth_type = models.AutoField(primary_key=True)
     name = models.CharField(max_length=45)
 
+    def __str__(self):
+        return self.name
+
 
 class Item(models.Model):
     id_item = models.AutoField(primary_key=True)
@@ -18,6 +21,9 @@ class Item(models.Model):
     color = models.CharField(max_length=45)
     cloth_type = models.ForeignKey(Cloth_type, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 
 class Client(models.Model):
     id_client = models.AutoField(primary_key=True)
@@ -28,8 +34,14 @@ class Client(models.Model):
     email = models.EmailField(max_length=254)
     birth_date = models.DateField(auto_now_add=True, null=False, blank=True)
 
+    def __str__(self):
+        return self.email
+
 
 class Orders(models.Model):
     id_order = models.AutoField(primary_key=True)
     id_client = models.ForeignKey(Client, on_delete=models.CASCADE)
     id_item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id_client
