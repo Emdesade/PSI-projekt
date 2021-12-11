eb7400adebbf88441d5ff13ed637e851a61f5d69
Wojciech <wojtekpako2@gmail.com>
2021-12-11 11:12:34 +0100

    View for Items (GET,POST)

--- PSI_projekt/clothes/views.py
+++ PSI_projekt/clothes/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import Item
+from .serializers import ItemSerializer
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+@csrf_exempt
+def item_list(request):
+    if request.method == 'GET':
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ItemSerializer(data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
